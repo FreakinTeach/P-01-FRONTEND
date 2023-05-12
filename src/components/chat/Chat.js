8401cc9cad389df42a1bc9dd1dd3909bd26b0b29
Chandru Angadi <83079815+Chandru779@users.noreply.github.com>
2023-05-12 20:22:20 +0530

    added chat code

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -22,42 +22,37 @@ import axios from "axios";
 const socket = io.connect("http://localhost:4000");
 
 export const Chat = () => {
-  const [messageTyped, setmessageTyped] = useState();
   const [users, setUsers] = useState();
-  const [selectedUser,setSelectedUser] = useState()
-  // const [outgoingMessages, setoutgoingMessages] = useState([
-  //   { name: "chandru", message: "Hey How are You man ?", time: new Date() },
-  // ]);
-  // const [incomingMessages, setincomingMessages] = useState([
-  //   { name: "Nithi", message: "Hey Super Machha !", time: new Date() },
-  // ]);
+  const [messageTyped, setmessageTyped] = useState();
+  const [selectedUser, setSelectedUser] = useState();
 
-  const [messages,setMessages] = useState([{type:'incoming',msg:{ name: "chandru", message: "Hey How are You man ?", time: new Date() }}])
+  const [messages, setMessages] = useState([]);
+  console.log(messages)
 
   const handleSendMessage = async () => {
-    console.log("sent_msg", messageTyped);
-
     if (messageTyped) {
       const msgdata = {
-        type:'outgoing',
-        msg:{
-        name: "chandru",
-        message: messageTyped,
-        time: new Date(),
-      }};
+        type: "outgoing",
+        msg: {
+          // name: {selectedUser.username},
+          id:selectedUser._id,
+          message: messageTyped,
+          time: new Date(),
+        },
+      };
       setMessages((prevmsg) => [...prevmsg, msgdata]);
       await socket.emit("send_message", msgdata);
     }
   };
 
-  const handleSelectedUser = (user)=>{
-    setSelectedUser(user)
-  }
+  const handleSelectedUser = (user) => {
+    setSelectedUser(user);
+  };
 
   useEffect(() => {
     socket.on("recieve_message", (data) => {
       console.log(data);
-      data.type = 'incoming'
+      data.type = "incoming";
       setMessages((prevmsg) => [...prevmsg, data]);
     });
   }, [socket]);
@@ -75,12 +70,13 @@ export const Chat = () => {
         <Sidebar position="left" scrollable={false}>
           <Search placeholder="Search..." />
           <ConversationList>
-            {users && users.length &&
+            {users &&
+              users.length &&
               users.map((user) => {
-                return(
+                return (
                   <Conversation
                     name={user.username}
-                    onClick={()=>handleSelectedUser(user)}
+                    onClick={() => handleSelectedUser(user)}
                     // lastSenderName="Chandru"
                     info={user.usermail}
                   ></Conversation>
@@ -88,56 +84,44 @@ export const Chat = () => {
               })}
           </ConversationList>
         </Sidebar>
-        <ChatContainer>
-          {selectedUser && <ConversationHeader>
-            <ConversationHeader.Back />
-            <ConversationHeader.Content
-              userName={selectedUser.username}
-              info="Active 10 mins ago"
-            />
-            <ConversationHeader.Actions>
-              <VoiceCallButton />
-              <VideoCallButton />
-              <InfoButton />
-            </ConversationHeader.Actions>
-          </ConversationHeader>}
-          <MessageList>
-            {messages &&
-              messages.length > 0 &&
-              messages.map((message) => {
-                return (
-                  <Message
-                    model={{
-                      message: message.msg.message,
-                      sentTime: message.msg.time,
-                      sender: message.msg.name,
-                      direction: message.type,
-                    }}
-                  />
-                );
-              })}
 
-            {/* {incomingMessages &&
-              incomingMessages.length > 0 &&
-              incomingMessages.map((message) => {
-                return (
-                  <Message
-                    model={{
-                      message: message.message,
-                      sentTime: message.time,
-                      sender: message.name,
-                      direction: "incoming",
-                    }}
-                  />
-                );
-              })} */}
-          </MessageList>
-          <MessageInput
-            placeholder="Type message here"
-            onChange={(val) => setmessageTyped(val)}
-            onSend={handleSendMessage}
-          />
-        </ChatContainer>
+        {selectedUser && (
+          <ChatContainer>
+            <ConversationHeader>
+              <ConversationHeader.Back />
+              <ConversationHeader.Content
+                userName={selectedUser.username}
+                info="Active 10 mins ago"
+              />
+              <ConversationHeader.Actions>
+                <VoiceCallButton />
+                <VideoCallButton />
+                <InfoButton />
+              </ConversationHeader.Actions>
+            </ConversationHeader>
+            <MessageList>
+              {messages &&
+                messages.length > 0 &&
+                messages.map((message) => {
+                  return (
+                    <Message
+                      model={{
+                        message: message.msg.message,
+                        sentTime: message.msg.time,
+                        sender: message.msg.name,
+                        direction: message.type,
+                      }}
+                    />
+                  );
+                })}
+            </MessageList>
+            <MessageInput
+              placeholder="Type message here"
+              onChange={(val) => setmessageTyped(val)}
+              onSend={handleSendMessage}
+            />
+          </ChatContainer>
+        )}
       </MainContainer>
     </div>
   );
