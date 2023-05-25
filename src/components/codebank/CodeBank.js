01aa26118bfae178be325a691eea2c456c21ed13
Pratik Chakraborty <chakrabortypratik2000@gmail.com>
2023-05-26 01:29:27 +0530

    codebank cards completed ui and config data

--- src/components/codebank/CodeBank.js
+++ src/components/codebank/CodeBank.js
@@ -1,23 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import "./codebank.css";
 import Header from "../landing/Header";
+import { codeBank } from "../config/cardconfig";
+import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
+import Modal from "@mui/material/Modal";
 import { useNavigate } from "react-router-dom";
 
 export const CodeBank = () => {
+  const [saveStatus, setSaveStatus] = useState(false);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const handleCardSave = () => {
+    setSaveStatus(!saveStatus);
+  };
+
   const navigate = useNavigate();
-  const userInfo = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user'))
+  const userInfo =
+    localStorage.getItem("user") && JSON.parse(localStorage.getItem("user"));
   return (
     <>
       {/* <Header /> */}
+      {/* Modal Show */}
+      
       <div className="cbcontainer">
         <div className="cbsidebar">
           <p onClick={() => userInfo && navigate("/chat")}>Chat</p>
           <p onClick={() => userInfo && navigate("/code")}>Compiler</p>
-          {!userInfo && <div className="signinlayer">
-            <button className="signinsidebar" onClick={()=>navigate('/auth')}>Sign in</button>
-          </div>}
+          {!userInfo && (
+            <div className="signinlayer">
+              <button
+                className="signinsidebar"
+                onClick={() => navigate("/auth")}
+              >
+                Sign in
+              </button>
+            </div>
+          )}
         </div>
-        <div className="cbsection">Code Bank Coming Soon ....</div>
+        <div className="cbsection">
+          <div className="cbsection-header">
+            <h2>Code Bank</h2>
+            <button className="codebank-post-btn" onClick={handleOpen}>
+              POST
+            </button>
+          </div>
+          <div className="codebank-grid-parent">
+            {/* <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            ></Modal> */}
+            
+            {codeBank && codeBank.length > 0
+              ? codeBank.map((i) => {
+                  let image = i.image;
+                  let title = i.title;
+                  let desc = i.description;
+                  let amount = i.amount;
+                  return (
+                    <>
+                      <div className="codebank-card">
+                        <img
+                          className="codebank-card-img"
+                          src={image}
+                          alt="question_img"
+                        ></img>
+                        <h5 className="codebank-card-title">{title}</h5>
+                        <p className="codebank-card-desc">{desc}</p>
+                        <p className="codebank-card-price">
+                          Bid Amount: ₹{amount}
+                        </p>
+                        <button className="codebank-card-button">ANSWER</button>
+                        <BsFillBookmarkFill
+                          onClick={handleCardSave}
+                          className="codebank-card-save-icon"
+                        />
+                      </div>
+                    </>
+                  );
+                })
+              : "No questions available now"}
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/components/config/cardconfig.js
@@ -0,0 +1,93 @@
+export const codeBank = [
+  {
+    id: "10",
+    title: "Help me develope a react based application",
+    description:
+      "I want to make a web app, that supports SEO and targetted marketting for an ecommerce store specially designed for setting digital goods",
+    image:
+      "https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    amount: 6000,
+    status: "active",
+  },
+  {
+    id: "1",
+    title: "Need assistance with front-end development",
+    description:
+      "I require help in developing the user interface for a web application using React.js.",
+    image: "https://images.unsplash.com/photo-1543966888-7c1dc482a810?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGphdmFzY3JpcHR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
+    amount: 1000,
+    status: "active",
+  },
+  {
+    id: "2",
+    title: "Backend development support required",
+    description:
+      "Looking for a developer who can assist in building the server-side logic for a web application.",
+    image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29kZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    amount: 2000,
+    status: "active",
+  },
+  {
+    id: "3",
+    title: "Design a responsive website using React",
+    description:
+      "I need a skilled designer who can create a responsive website using React.js framework.",
+    image: "https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNvZGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
+    amount: 1500,
+    status: "active",
+  },
+  {
+    id: "4",
+    title: "Implement SEO for an existing website",
+    description:
+      "I'm looking for an expert who can optimize my website for search engines and improve its ranking.",
+    image: "https://images.unsplash.com/photo-1546900703-cf06143d1239?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvZGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
+    amount: 3000,
+    status: "active",
+  },
+  {
+    id: "5",
+    title: "Develop an eCommerce store for digital goods",
+    description:
+      "I want to create an online store specifically for selling digital products. Need assistance in building it.",
+    image: "https://media.istockphoto.com/id/1284271878/photo/javascript-inscription-against-laptop-and-code-background-learn-javascript-programming.webp?b=1&s=170667a&w=0&k=20&c=pNyw5iN2ee3oK5adyRBr9-w1cPh9hkUGGnc5Q4JfyJM=",
+    amount: 5000,
+    status: "active",
+  },
+  {
+    id: "6",
+    title: "Build a mobile app using React Native",
+    description:
+      "Require a developer who can build a mobile application for both iOS and Android platforms using React Native.",
+    image: "https://media.istockphoto.com/id/1363276509/photo/teacher-giving-computer-science-lecture-to-diverse-multiethnic-group-of-female-and-male.webp?b=1&s=170667a&w=0&k=20&c=MEf-p6PoemvTnsK5_qu940BMMu3vJf_ElurJQ5jqcuQ=",
+    amount: 4000,
+    status: "active",
+  },
+  {
+    id: "7",
+    title: "Create a visually appealing landing page",
+    description:
+      "Looking for a designer to create an eye-catching landing page design for a new website.",
+    image: "https://media.istockphoto.com/id/1432809744/photo/computer-programmer-working-on-new-software-program.webp?b=1&s=170667a&w=0&k=20&c=kEJMRIDTI0kV9g7ML_PYkNngdO03mvWGX3kvGawr6mY=",
+    amount: 2500,
+    status: "active",
+  },
+  {
+    id: "8",
+    title: "Develop a custom CMS for content management",
+    description:
+      "I need a developer who can build a custom CMS to manage content on my website.",
+    image: "https://media.istockphoto.com/id/1450126135/photo/backup-data-on-various-devices.webp?b=1&s=170667a&w=0&k=20&c=LsFKxfcvbHe0sPq3Bi9SJfR3ZiddjUKoBQEhRKMo--8=",
+    amount: 3500,
+    status: "active",
+  },
+  {
+    id: "9",
+    title: "Design and implement user authentication system",
+    description:
+      "Require a developer to design and implement a secure user authentication system for a web application.",
+    image: "https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGphdmFzY3JpcHR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
+    amount: 1800,
+    status: "active",
+  },
+];
