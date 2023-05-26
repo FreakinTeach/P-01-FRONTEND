454472c72fdff5748b80f3ce191afa58d94810c0
Chandru Angadi <83079815+Chandru779@users.noreply.github.com>
2023-05-27 02:03:21 +0530

    new landing page added and css changes all parts

--- src/components/landing/Header.js
+++ src/components/landing/Header.js
@@ -1,30 +1,38 @@
 import React from "react";
-import './header.css';
+import "./header.css";
 import { useNavigate } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
 
-function Header(){
-    const navigate = useNavigate()
-    let getUser = localStorage.getItem('user')
-    const user = getUser && JSON.parse(getUser) 
-    console.log(user)
+function Header() {
+  const navigate = useNavigate();
+  let getUser = localStorage.getItem("user");
+  const user = getUser && JSON.parse(getUser);
+  console.log(user);
 
-    return(
-        <div className="headercontainer">
-                <ul className="headerleft">
-                    <li onClick={()=>navigate('/')}>Freakintech</li>
-                    <li>Services</li>
-                    <li>Features</li>
-                    <li>Pricing</li>
-                    <li>News</li>
-                </ul>
-                <div className="headerright">
-                    {user && Object.values(user).length > 0 ? <img src="https://cdn.dribbble.com/users/1223630/screenshots/8115260/media/8145a871d9c4d67ec06e047ccc6574b4.gif" />
-                    :
-                    <button className="siginbtn" onClick={()=>navigate('/auth')}>Sign in</button>
-                    }
-                </div>
-        </div>
-    )
+  return (
+    <div className="headercontainer">
+      <p className="headerbrandlogo" onClick={() => navigate("/")}>
+        FreakinTech
+      </p>
+      <ul className="headerleft">
+        <li>Home</li>
+        <li>Features</li>
+        <li>Services</li>
+        <li>Pricing</li>
+        <li>About</li>
+      </ul>
+      <div className="headerright">
+        {user && Object.values(user).length > 0 ? (
+          <FaUserCircle style={{ color: "#ffffff", fontSize: "24px" }} />
+        ) : (
+          // <img src="https://cdn.dribbble.com/users/1223630/screenshots/8115260/media/8145a871d9c4d67ec06e047ccc6574b4.gif" />
+          <button className="siginbtn" onClick={() => navigate("/auth")}>
+            Sign in
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/components/landing/Main.js
+++ src/components/landing/Main.js
@@ -2,35 +2,62 @@ import React from "react";
 import "./main.css";
 import Header from "./Header";
 import { useNavigate } from "react-router-dom";
+import landingImage from '../../assets/landing.png'
 
 export const Main = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <>
       {/* <Header /> */}
       <div className="maincontainer">
-        <div className="maincontent">
-          <h1>
-            Simple way<br /> to control <br/>
-            <span>your savings.</span>
-          </h1>
-          <p>
-            Give all your customers global freedom
-            <br /> with the top of their card
-          </p>
+        <section className="lpsection1">
+          <div className="maincontent">
+            <h1>
+              {/* Simple way<br /> to control <br/> */}
+              {/* <span>your savings.</span> */}
+              Unlock the Secrets of <br />
+              Masterful Coding with <br />
+              <span>FreakinTech</span>
+            </h1>
+            <p>
+              You will meet the most intresting techy's here
+              <br /> Hire Teach Learn Earn
+            </p>
 
-          <div className="explorebtn">
-            {/* <input type="text" placeholder="Enter your email" /> */}
-            <button className="btnmain" onClick={()=>navigate('/codebank')}>Explore</button>
+            <div className="explorebtn">
+              {/* <input type="text" placeholder="Enter your email" /> */}
+              <button className="btnmain" onClick={() => navigate("/codebank")}>
+                Get Started
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="imagesection">
-          <img
-            src="https://images.unsplash.com/photo-1562813733-b31f71025d54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZ3JhbW1lcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-            alt="simple-image"
-          />
-        </div>
+          <div className="imagesection">
+            <img
+              src={landingImage}
+              alt="simple-image"
+            />
+          </div>
+        </section>
+
+        <section className="lpsection2">
+          <div>
+            <p>290+</p>
+            <span>Techy's Joined</span>
+          </div>
+          <div>
+            <p>3290+</p>
+            <span>Code Assets</span>
+          </div>
+          <div>
+            <p>290+</p>
+            <span>Client Satisfaction Rate</span>
+          </div>
+          <div>
+            <p>17+</p>
+            <span>Tech Products</span>
+          </div>
+        </section>
       </div>
     </>
   );
