2c8d3f675df2a99fb376d36d573102593ab2c40f
Chandru Angadi <83079815+Chandru779@users.noreply.github.com>
2023-06-12 21:20:59 +0530

    config changes

--- src/config/cardconfig.js
+++ src/config/cardconfig.js
@@ -8,6 +8,7 @@ export const codeBank = [
       "https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
     amount: 6000,
     status: "active",
+    skills:['React','AWS','Node']
   },
   {
     id: "1",
@@ -17,6 +18,7 @@ export const codeBank = [
     image: "https://images.unsplash.com/photo-1543966888-7c1dc482a810?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGphdmFzY3JpcHR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
     amount: 1000,
     status: "active",
+    skills:['React','Javascript','Node']
   },
   {
     id: "2",
@@ -26,6 +28,7 @@ export const codeBank = [
     image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29kZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
     amount: 2000,
     status: "active",
+    skills:['React','Next','Node']
   },
   {
     id: "3",
@@ -35,6 +38,7 @@ export const codeBank = [
     image: "https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNvZGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
     amount: 1500,
     status: "active",
+    skills:['Python','Javascript','Node']
   },
   {
     id: "4",
@@ -44,6 +48,7 @@ export const codeBank = [
     image: "https://images.unsplash.com/photo-1546900703-cf06143d1239?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvZGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
     amount: 3000,
     status: "active",
+    skills:['Java','Javascript','Node']
   },
   {
     id: "5",
@@ -53,6 +58,7 @@ export const codeBank = [
     image: "https://media.istockphoto.com/id/1284271878/photo/javascript-inscription-against-laptop-and-code-background-learn-javascript-programming.webp?b=1&s=170667a&w=0&k=20&c=pNyw5iN2ee3oK5adyRBr9-w1cPh9hkUGGnc5Q4JfyJM=",
     amount: 5000,
     status: "active",
+    skills:['Docker','Javascript','Node']
   },
   {
     id: "6",
@@ -62,6 +68,7 @@ export const codeBank = [
     image: "https://media.istockphoto.com/id/1363276509/photo/teacher-giving-computer-science-lecture-to-diverse-multiethnic-group-of-female-and-male.webp?b=1&s=170667a&w=0&k=20&c=MEf-p6PoemvTnsK5_qu940BMMu3vJf_ElurJQ5jqcuQ=",
     amount: 4000,
     status: "active",
+    skills:['AWS','Javascript','Docker']
   },
   {
     id: "7",
@@ -71,6 +78,7 @@ export const codeBank = [
     image: "https://media.istockphoto.com/id/1432809744/photo/computer-programmer-working-on-new-software-program.webp?b=1&s=170667a&w=0&k=20&c=kEJMRIDTI0kV9g7ML_PYkNngdO03mvWGX3kvGawr6mY=",
     amount: 2500,
     status: "active",
+    skills:['Reactleaflet','Javascript','Node']
   },
   {
     id: "8",
@@ -80,6 +88,7 @@ export const codeBank = [
     image: "https://media.istockphoto.com/id/1450126135/photo/backup-data-on-various-devices.webp?b=1&s=170667a&w=0&k=20&c=LsFKxfcvbHe0sPq3Bi9SJfR3ZiddjUKoBQEhRKMo--8=",
     amount: 3500,
     status: "active",
+    skills:['React','Javascript','Node']
   },
   {
     id: "9",
@@ -89,5 +98,6 @@ export const codeBank = [
     image: "https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGphdmFzY3JpcHR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
     amount: 1800,
     status: "active",
+    skills:['React','Java','.net']
   },
 ];
