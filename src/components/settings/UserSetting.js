066b65d5c00ec3c9955c363f90e334b19d1fcf84
NITHISHGOWDA98 <nithishgowda98@gmail.com>
2023-06-16 01:12:09 +0530

    settings pending

--- src/components/settings/UserSetting.js
+++ src/components/settings/UserSetting.js
@@ -1,12 +1,43 @@
 import React from "react";
+import { TSHeader } from "../techstack/TSHeader";
 import "./usersetting.css";
 
 export const UserSetting = () => {
   return (
     <div className="uscontainermain">
-      <div className="uscontainerheader" >
-       <b> Welcome to Settings</b> 
+      <TSHeader layout={"Welcome to Settings"} />
+      <div className="settings1">
+        <div className="settings11">
+          <b>Personal information</b>
+        </div>
+        <div className="settings12">
+          <div className="settings121">
+            Your Avatar
+          </div>
+          <div className="settings122">
+          <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU"
+          width="50px"
+          height="50px"
+        />
+        
+        <div className="settings123">
+          chandru
+          <div className="settings1231">
+           <a href="#">chandru@gmail.com</a> 
+          </div>
+        </div>
+        <div className="settings1232">
+          <button>
+          Upload now
+          </button>
+
+        </div>
+          </div>
+         
+        </div>
       </div>
     </div>
+
   );
 };
